d2fb4a40f7257a6b1cee132e441b548ab5db0e6e
terrencejihoonjung <terrencejung928@gmail.com>
2023-07-31 16:27:24 -0700

    learned about module wrappers and caching

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-require("./batman");
-require("./superman");
+const SuperHero = require("./super-hero");
+
+const batman = new SuperHero("Batman");
+const superman = new SuperHero("Superman");
+
+console.log(batman.getName());
+console.log(superman.getName());
+
+batman.setName("Bruce Wayne");
+superman.setName("Super Man");
+
+console.log(batman.getName());
+console.log(superman.getName());
