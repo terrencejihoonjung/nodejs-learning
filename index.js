95ec84861406b4e8c33e4b5d3c1b313e89766056
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-03 14:52:36 -0700

    learned about fs module and fs promise modules

--- index.js
+++ index.js
@@ -31,3 +31,16 @@
 //     console.log("File written");
 //   }
 // });
+
+const fs = require("node:fs/promises");
+
+async function readFile() {
+  try {
+    const data = await fs.readFile("file.txt", "utf-8");
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+readFile();
