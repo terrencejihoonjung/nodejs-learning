dfe1b556123faee8abbb9e1c5c25aea3f0587631
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-03 15:45:55 -0700

    learned about streams module and pipes

--- index.js
+++ index.js
@@ -1,46 +1,20 @@
-// const fs = require("node:fs");
+const fs = require("node:fs");
+const zlib = require("node:zlib");
 
-// console.log("first");
-// // read the contents of a file
-// const fileContents = fs.readFileSync("./file.txt", "utf-8");
-// console.log(fileContents);
+const gzip = zlib.createGzip();
 
-// console.log("second");
+const readableStream = fs.createReadStream("./file.txt", {
+  encoding: "utf-8",
+  highWaterMark: 2,
+});
 
-// // asynchronous reading
-// // if there is no error, error = null
-// // data is populated with the file contents
-// fs.readFile("./file.txt", "utf-8", (error, data) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(data);
-//   }
+readableStream.pipe(gzip).pipe(fs.WriteStream("./file2.txt.gz"));
+
+const writeableStream = fs.createWriteStream("./file2.txt");
+
+readableStream.pipe(writeableStream);
+
+// readableStream.on("data", (chunk) => {
+//   console.log(chunk);
+//   writeableStream.write(chunk);
 // });
-
-// console.log("third");
-
-// // Sync
-// fs.writeFileSync("./greet.txt", "Hello world!");
-
-// // Async
-// fs.writeFile("./greet.txt", " Hello world!", { flag: "a" }, (error) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log("File written");
-//   }
-// });
-
-const fs = require("node:fs/promises");
-
-async function readFile() {
-  try {
-    const data = await fs.readFile("file.txt", "utf-8");
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-readFile();
