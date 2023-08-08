4653e3931f386e521c6453f0d1bc7826374e21d5
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-07 23:48:14 -0700

    learned about HTTP and JSON responses

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 /* 
   function that extends the event emitter class receives requests and sends responses
@@ -7,9 +8,10 @@ const http = require("node:http");
 */
 const server = http.createServer((req, res) => {
   // HTTP response header
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.createReadStream(__dirname + "/index.html").pipe(res);
   // End the response
-  res.end("Hello World");
+  // res.end(html);
 });
 
 // have server listen for requests that takes in a port number => localhost:3000 on browser
