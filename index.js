aed4094309d2ed84fdcdaf1cea342c04a19ddaf6
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-08 00:00:06 -0700

    learned about HTTP routing

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ const fs = require("node:fs");
   after the request, the callback function builds the response to send back to the client
 */
 const server = http.createServer((req, res) => {
-  const name = "Terrence";
-  res.writeHead(200, { "Content-Type": "text/html" });
-  let html = fs.readFileSync("./index.html", "utf-8");
-  html = html.replace("{{name}}", name);
-  res.end(html);
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home Page");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("About Page");
+  } else if (req.url === "/api") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(JSON.stringify({ firstName: "Bruce", lastName: "Wayne" }));
+  } else {
+    res.writeHead(404);
+    res.end("Page not found");
+  }
 });
 
 // have server listen for requests that takes in a port number => localhost:3000 on browser
