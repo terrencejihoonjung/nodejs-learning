641df27b36c5b124d9a1fde93c48b2d72ed8e3b3
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-08 01:28:30 -0700

    learned about network I/O

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const crypto = require("node:crypto");
+// const crypto = require("node:crypto");
+// process.env.UV_THREADPOOL_SIZE = 9;
 
-process.env.UV_THREADPOOL_SIZE = 9;
-const MAX_CALLS = 9;
+const https = require("node:https");
+const MAX_CALLS = 12;
 
 const start = Date.now();
 for (let i = 0; i < MAX_CALLS; i++) {
-  crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
-    console.log(`Hash: ${i + 1}`, Date.now() - start);
-  });
+  // crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
+  //   console.log(`Hash: ${i + 1}`, Date.now() - start);
+  // });
+
+  https
+    .request("https://www.google.com", (res) => {
+      res.on("data", () => {});
+      res.on("end", () => {
+        console.log(`Request: ${i + 1}`, Date.now() - start);
+      });
+    })
+    .end();
 }
