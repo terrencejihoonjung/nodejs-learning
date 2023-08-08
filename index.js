f55f58ed4912bf2a06174b22efca28f5b92d4efd
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-08 01:15:00 -0700

    learned about libuv and thread pools

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-const http = require("node:http");
-const fs = require("node:fs");
+const crypto = require("node:crypto");
 
-/* 
-  function that extends the event emitter class receives requests and sends responses
-  whenever a request is sent to the server, this callback function is executed
-  after the request, the callback function builds the response to send back to the client
-*/
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Home Page");
-  } else if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("About Page");
-  } else if (req.url === "/api") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(JSON.stringify({ firstName: "Bruce", lastName: "Wayne" }));
-  } else {
-    res.writeHead(404);
-    res.end("Page not found");
-  }
-});
+process.env.UV_THREADPOOL_SIZE = 9;
+const MAX_CALLS = 9;
 
-// have server listen for requests that takes in a port number => localhost:3000 on browser
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+const start = Date.now();
+for (let i = 0; i < MAX_CALLS; i++) {
+  crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
+    console.log(`Hash: ${i + 1}`, Date.now() - start);
+  });
+}
