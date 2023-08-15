1a9ba217dadd63a58847727db0d76e4ab4c00fd4
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-15 16:34:45 -0700

    learned about publish npm packages

--- my-custom-package/index.js
+++ my-custom-package/index.js
@@ -1,7 +1,7 @@
 const upperCase = require("upper-case").upperCase;
 
 function greet(name) {
-  console.log(upperCase(`Hello ${name}, welcome to Codevolution`));
+  console.log(upperCase(`Hello ${name}, welcome to Codevolution!`));
 }
 
 greet("Terrence");
