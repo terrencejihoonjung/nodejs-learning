67cdac61b31d5b4a09b7454d2bd933d7ed0bead8
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-14 19:05:05 -0700

    learned about check queues and close queues

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 
-fs.readFile(__filename, () => {
-  console.log("this is readFile 1");
+const readableStream = fs.createReadStream(__filename);
+readableStream.close();
+
+readableStream.on("close", () => {
+  console.log("this is from readableStream close event callback");
 });
 
-process.nextTick(() => console.log("this is process.nextTick 1"));
-Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+setImmediate(() => console.log("this is setImmediate 1"));
 setTimeout(() => console.log("this is setTimeout 1"), 0);
-
-for (let i = 0; i < 2000000; i++) {}
+Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+process.nextTick(() => console.log("this is process.nextTick 1"));
