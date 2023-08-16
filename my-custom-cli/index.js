24b34fc6ffb11bf6e065529b4197053b4a4c6897
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-15 22:52:25 -0700

    added CLI options

--- my-custom-cli/index.js
+++ my-custom-cli/index.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log("codevolution pokedex");
+const yargs = require("yargs");
+const { argv } = yargs(process.argv);
+
+const printFiveMoves = async (pokemonName) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  );
+
+  const pokemon = await response.json();
+  const moves = pokemon.moves.map(({ move }) => move.name);
+  console.log(moves.slice(0, 5));
+};
+
+printFiveMoves(argv.pokemon);
