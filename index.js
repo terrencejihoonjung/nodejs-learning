5e27c4074194535a2e9096cfa4aba3a41d039dab
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-16 17:44:01 -0700

    change index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text.plain" });
-  res.end("Hello World!");
+  res.end("MWAH!");
 });
 
 const PORT = process.env.PORT || 3000;
